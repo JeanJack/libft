3fd45c371fdd4e500c44e74367e27f5038d12979
root <root@TheJeanJackPC-Portable.localdomain>
2018-11-21 12:37:53 +0100

    rien

--- ft_lstadd.c
+++ ft_lstadd.c
@@ -15,5 +15,7 @@
 
 void	ft_lstadd(t_list **alst, t_list *new)
 {
-	
+	if (*alst != NULL)
+		new->next = alst;
+	*alst = new;
 }
--- ft_lstdel.c
+++ ft_lstdel.c
@@ -15,5 +15,14 @@
 
 void	ft_lstdel(t_list **alst, void (*del)(void *, size_t))
 {
-	
+	t_list *lst;
+
+	while (*alst)
+	{
+		lst = *alst->next;
+		del(*alst->content, *alst->content_size);
+		free(*alst);
+		*alst = lst;
+	}
+	*alst = NULL;
 }
