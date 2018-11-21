e9ffea74cb9bcc768d6162aaab2451a5842ae84e
root <root@TheJeanJackPC-Portable.localdomain>
2018-11-21 12:28:02 +0100

    rien

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -15,15 +15,23 @@
 
 t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
 {
+	t_list *new;
 	t_list *list;
+	t_list *elem;
 
+	elem = f(lst);
+	if (!(new = ft_lstnew(elem->content, elem->content_size)))
+		return (NULL);
+	lst = lst->next;
+	list = new;
 	while (lst)
 	{
-		if (!(list = (t_list*)malloc(sizeof())))
+		elem = f(lst);
+		if (!(new->next = ft_lstnew(elem->content, elem->content_size)))
 			return (NULL);
-		list->content = f(lst);
-		list = list->next;
+		new = new->next;
 		lst = lst->next;
 	}
 	return (list);
+
 }
